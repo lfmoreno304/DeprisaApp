590f77cc72a089f9e332a80ad320f36c26dcc64d
Luis <luifer0115@gmail.com>
2021-12-07 14:41:17 -0500

    formulario de administrador para usuarios y mensajeros

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import "./Styles/index.css";
 import Login from "./Components/Login";
 import Home from "./Components/Home";
 import Tracking from "./Components/Tracking";
-import Login from "./Components/Login";
-import AdminForm from "./Components/AdminForm";
 import PackageForm from "./Components/PackageForm";
 import MyDeliveries from "./Components/MyDeliveries";
 import MyDeliveries2 from "./Components/MyDeliveries2";
 import UserTracking from "./Components/UserTracking";
-
+import UserProfile from "./Components/UserProfile";
+import EmployeeProfile from "./Components/EmployeeProfile";
+import PackageFormComp from "./Components/PackageFormComp";
+import Delivered from  "./Components/Delivered";
+import Scheduled from "./Components/Scheduled";
+import UserManagement from "./Components/UserManagement";
+import EmployeeManagement from "./Components/EmployeeManagement";
 export default function App() {
 
   return (
@@ -32,6 +36,8 @@ export default function App() {
         <Route path="/mydeliveries" element={<MyDeliveries />} />
         <Route path="/mydeliveries2" element={<MyDeliveries2 />} />
         <Route path="/usertracking" element={<UserTracking />} />
+        <Route path="/usermanagement" element={<UserManagement/>}/>
+        <Route path="/employeemanagement" element={<EmployeeManagement/>}/>
       </Routes>
       </div>
     </Router>    
--- src/Components/EmployeeManagement.jsx
+++ src/Components/EmployeeManagement.jsx
@@ -1,46 +1,55 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
-import SideBar from './SideBar'
-export default function AdminForm() {
+import React, { useEffect, useState } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import SideBar from './SideBar';
+import axios from 'axios';
+
+export default function EmployeeManagement() {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        const url = "http://localhost:3004/employee";
+        axios.get(url).then(function (res) {
+            setUsers(res.data);
+            console.log("Ok", res.data)
+        }).catch(function (err) {
+            console.log("Error", err)
+        })
+    }, []);
     return (
         <>
             <Header />
             <div className="d-flex">
                 <SideBar />
                 <div className="container">
-                    <h1 className="text-center text-warning pt-5 mb-5">Gestion de usuarios</h1>
+                    <h1 className="text-center text-warning pt-5 mb-5">Gestion de mensajeros</h1>
                     <table className="table">
                         <thead>
                             <tr>
                                 <th scope="col">#</th>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
-                                <th scope="col">Handle</th>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Ciudad</th>
+                                <th scope="col">Direccion</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td colspan="2">Larry the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
+                            {users.map(user => {
+                                return (
+                                    <>
+                                        <tr scope="row" key={user.id}>
+                                            <td>{user.id}</td>
+                                            <td>{user.name}</td>
+                                            <td>{user.email}</td>
+                                            <td>{user.city}</td>
+                                            <td>{user.adress}</td>
+                                        </tr>
+                                    </>
+                                )
+                            })}
                         </tbody>
                     </table>
                     <h1 className="text-center text-warning pt-5 mb-5">Nuevo registro</h1>
-                   <form>
+                    <form>
                         <div className="row mb-3">
                             <div className="col">
                                 <input className="form-control" type="text" name="" id="" placeholder="nombre" />
@@ -68,7 +77,7 @@ export default function AdminForm() {
                         <div className="d-grid gap-2 col-2 mx-auto pb-3 pt-4  mb-3">
                             <button type="submit" className="btn btn-primary ">Registrar</button>
                         </div>
-                   </form>
+                    </form>
                 </div>
             </div>
             <Footer />
