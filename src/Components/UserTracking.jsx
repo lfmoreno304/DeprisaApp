b69caca89f6dd5fb363cb044c3e9e215668a4eea
Andrea Velandia <77517773+andrea-vel@users.noreply.github.com>
2021-12-11 10:52:52 -0500

    Update UserTracking.jsx

--- src/Components/UserTracking.jsx
+++ src/Components/UserTracking.jsx
@@ -9,18 +9,18 @@ export default function UserTracking() {
             <Header />
             <div className="d-flex">
                 <SideBar/>
-                <div className="container"> 
+                <div className="container pb-1 pt-4"> 
                     <h1 className="text-center text-warning pt-5">RASTREAR MI ENVÍO</h1>
                     <p className="col text-center">Realice el seguimiento de sus envios Nacionales</p>
                     <p className="col text-center">Ingrese su número de Guía</p>   
 
-                    <div className="container">             
-                        <div className="row mb-3">
-                            <div className="col">
-                            <input className="form-control text-left" placeholder="N° de Seguimiento"></input>
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-sm-8">
+                               <input className="form-control text-left" placeholder="N° de Seguimiento"></input>
                             </div>
-                            <div className="col">
-                                <button className="btn btn-primary col-lg-3" onClick="">Consultar </button>
+                            <div className="col-sm-4">
+                                <button className="btn btn-primary col-lg-4" onClick="">Consultar </button>
                             </div>
                         </div>          
                     </div>
